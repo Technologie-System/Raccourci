8508b253d4714dc4bbde9c67f8b75c960d9e8ceb
Thibault Savenkoff <66521530+Thibault-Savenkoff@users.noreply.github.com>
2021-08-21 18:39:47 +0200

    Add files via upload

--- Raccourci/Credits.vb
+++ Raccourci/Credits.vb
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 ﻿Public Class Credits
     Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
         Process.Start("https://github.com/Thibault-Savenkoff")
@@ -10,4 +11,24 @@
     Private Sub LinkLabel2_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel2.LinkClicked
         Process.Start("https://github.com/Technologie-System")
     End Sub
+=======
+﻿Public Class Credits
+    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        Process.Start("https://github.com/Thibault-Savenkoff")
+    End Sub
+
+    Private Sub Credits_FormClosed(sender As Object, e As FormClosedEventArgs) Handles Me.FormClosed
+        Me.Hide()
+        Main.Show()
+    End Sub
+
+    Private Sub LinkLabel2_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel2.LinkClicked
+        Process.Start("https://github.com/Technologie-System")
+    End Sub
+
+    Private Sub LinkLabel3_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel3.LinkClicked
+        Process.Start("https://icones8.fr")
+    End Sub
+
+>>>>>>> Stashed changes
 End Class
